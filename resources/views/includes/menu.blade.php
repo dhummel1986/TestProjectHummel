91482a0856c09b2023d465ac9e44cba5e832accc
diana <dpa6@njit.edu>
2018-03-27 21:27:21 -0400

    Contact Page:Controller, view and Route (3rd commit)

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -5,9 +5,16 @@
             <a class="navbar-brand" href="#">WebSiteName</a>
         </div>
         <ul class="nav navbar-nav">
-            <li class="active"><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-
+            <li class="nav-item active">
+                <a class="nav-link" href="{{route('home')}}">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('about')}}">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('contact')}}">Contact</a>
+            </li>
+            </li>
         </ul>
     </div>
 </nav>
