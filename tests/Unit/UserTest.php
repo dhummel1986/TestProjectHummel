cf173523083d4fa2a2d16a0a64c73740fa3855ee
diana <dpa6@njit.edu>
2018-03-29 18:24:21 -0400

    Update a user name via unit test (16th commit)

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -14,17 +14,21 @@ class UserTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
+
+    public function testinsert()
     {
         $user = new User();
-        $user->name = 'Diana';
+        $user->name = 'Diana Hummel';
         $user->email= 'diana@yahoo.com';
-            $user->password= '123test';
+        $user->password= '123test';
         $this->assertTrue($user->save());
-
-
     }
-
+    public function testupdate()
+    {
+        $user = User::find(12);
+        $user->Name = 'Steve Smith';
+        $this->assertTrue($user->save());
+    }
 
 }
 
