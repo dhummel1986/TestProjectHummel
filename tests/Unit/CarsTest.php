302031f4b5338ef66704d7834f3abc84c755cf39
diana <dpa6@njit.edu>
2018-03-29 20:14:26 -0400

    Delete of car (17th commit)

--- tests/Unit/CarsTest.php
+++ tests/Unit/CarsTest.php
@@ -9,24 +9,13 @@ use App\Cars;
 
 class CarsTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testinsert()
-    {
-        $cars = new cars();
-        $cars->Make = 'Chevrolet';
-        $cars->Model= 'Impala';
-        $cars->Year= '2030';
-        $this->assertTrue($cars->save());
+    public function testdelete()
+{
+        $cars = Cars::find(51);
+        $cars-> Make = 'Chevrolet';
+        $cars-> Model= 'Impala';
+        $cars-> Year= '2030';
+        $cars->save();
+        $this->assertTrue($cars->delete());
     }
-    public function testupdate()
-    {
-        $cars = Cars::find(12);
-        $cars->Year = '2000';
-        $this->assertTrue($cars->save());
-    }
-
 }
