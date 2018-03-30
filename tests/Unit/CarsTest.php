ab9b98068867c96f43fdde7336865939163d0c30
diana <dpa6@njit.edu>
2018-03-29 20:16:59 -0400

    count of cars (18th commit)

--- tests/Unit/CarsTest.php
+++ tests/Unit/CarsTest.php
@@ -5,17 +5,23 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\Constraint\IsType;
 use App\Cars;
 
 class CarsTest extends TestCase
 {
     public function testdelete()
 {
-        $cars = Cars::find(51);
+        $cars = Cars::find(53);
         $cars-> Make = 'Chevrolet';
         $cars-> Model= 'Impala';
         $cars-> Year= '2030';
         $cars->save();
         $this->assertTrue($cars->delete());
+    }
+    public function testCollectionCount(){
+        $cars= Cars::All();
+        $recordCount = $cars->count();
+        $this->assertInternalType(IsType::TYPE_INT, $recordCount );
     }
 }
